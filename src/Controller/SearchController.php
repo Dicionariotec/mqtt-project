cea080fa12ebe7284c143c439d1a3f7a7a612758
italo ricardo geske <italoricardogeske@gmail.com>
2022-10-16 22:44:54 +0900

    一旦

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Elastic\Elasticsearch\ClientBuilder;
 
 class SearchController extends AbstractController
 {
@@ -31,11 +32,26 @@ class SearchController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
+            return $this->renderForm('search/search.html.twig', [
+                'form' => $form,
+                'results' => $this->runElasticSearch(data: $data)
+            ]);
         }
 
         return $this->renderForm('search/search.html.twig', [
             'form' => $form,
+            'results' => []
         ]);
+    }
+
+    private function runElasticSearch(array $data) {
+        $client = ClientBuilder::create()
+            ->setElasticCloudId('dicionariotec:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDk0NDViYzczMWQxNDRkODZiYmJmZTQ4ZmRlNTg3MjgxJGY4OWI3NjgzYTY4YzRjYzZiZTlkYmRlZWIyNTA3Y2E2')
+            ->setApiKey('SmNzSDRJTUJYcjdpeHdzY3RkOEs6MVZwMFR0aW1TNGlrU3hCekROMEhfQQ==')
+            ->build();
+
+        // Info API
+        return $client->info();
     }
 
 }
