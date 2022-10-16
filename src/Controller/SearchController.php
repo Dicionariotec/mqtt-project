b6578cf82af0d8c9dfa14f06ae05fd87865afb6b
italo ricardo geske <italoricardogeske@gmail.com>
2022-10-16 17:08:40 +0900

    assets

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,14 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'app_search')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+        $defaultData = ['message' => 'Digite o que você procura'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('keyword', TextType::class, [
+                'label' => 'Digite aqui para pesquisar',
+                'attr' => [
+                    'placeholder' => 'Digite aqui para pesquisar',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Pesquisar'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+        }
+
+        return $this->renderForm('search/search.html.twig', [
+            'form' => $form,
         ]);
     }
+
 }
